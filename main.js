8e2e181796e7f660d8d9e5d01e1278b7ca78dba6
SonitMuhil <77870888+SonitMuhil@users.noreply.github.com>
2022-03-19 07:44:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+img = "";
+status = "";
+
+function preload(){
+ img = loadImage('background.jpg');
+}
+
+
+
+function nextScreen(){
+    window.location="index1.html";
+}
+function nextScree1(){
+    window.location="index2.html";
+}
+function nextScreen2(){
+    window.location="index3.html";
+}
+function nextScreen3(){
+    window.location="index4.html";
+}
+function nextScreen4(){
+    window.location="index5.html";
+}
